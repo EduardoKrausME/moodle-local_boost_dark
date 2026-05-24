ca94df73952cd41b84b9146707ec9346bc97d9db
Eduardo Kraus <kraus@eduardokraus.com>
2026-05-24 20:01:14 -0300

    @license

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -19,7 +19,7 @@
  *
  * @package   local_boost_dark
  * @copyright 2024 Eduardo Kraus {@link https://eduardokraus.com}
- * @license   http://www.gnu.org/boost_darkleft/gpl.html GNU GPL v3 or later
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 namespace local_boost_dark\privacy;
